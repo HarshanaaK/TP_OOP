ec0c29ac0a99cdd5847f137398a4d7c0df80b676
HarshanaaK <harshanaakengadarane@gmail.com>
2026-09-15 15:25:34 +0200

    Question 3 : tuples pour le journal de bord du robot

--- tp0/tuples.py
+++ tp0/tuples.py
@@ -3,7 +3,7 @@ def afficher_releve (reserve):
     valeur = reserve[1]
     unite = reserve[2]
     return f"Capteur {nom_capteur} : {valeur} {unite}"
-    
+
 releve1 = ("laser_avant", 2.35, "m")
 releve2 = ("laser_arriere", 1.10, "m")
 releve3 = ("gyroscope", 87.5, "deg")
@@ -12,3 +12,17 @@ releves = [releve1, releve2, releve3]
 assert len(releves) == 3
 assert releves[0][0] == "laser_avant"
 assert afficher_releve(releve1) == "Capteur laser_avant : 2.35 m"
+
+def recalibrer(releve,nom_capteur,valeur):
+    for i in range (3):
+        if ( releve [i][0] == nom_capteur) :
+            nouveaux_reserves = (releve[i][0],valeur,releve[i][2])
+            releve[i]=nouveaux_reserves
+            
+    return releve
+
+nouveaux_releves = recalibrer(releves, "laser_avant", 2.40)
+assert nouveaux_releves[0] == ("laser_avant", 2.40, "m")
+assert nouveaux_releves[1] == releve2
+assert nouveaux_releves[2] == releve3
+    
